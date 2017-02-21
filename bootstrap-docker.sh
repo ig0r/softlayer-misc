8d6a1ae698f999a7761104dd0941ce4f9cd77cdb
Igor Sukharev <igor.sukharev@gmail.com>
2017-02-21 21:15:23 +0300

    adding a bootstrap script for docker
    - installs the latest docker
    - installs the latest docker-compose

---
+++ bootstrap-docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+###############################################################
+#
+# Script for bootstrapping a softlayer vm with docker
+#
+# Softlayer nodes have a root user enabled by default
+#
+###############################################################
+
+echo `date` bootstrapping node
+
+echo `date` installing docker
+
+apt-get update && apt-get -y install linux-image-extra-$(uname -r) curl apt-transport-https
+curl -L https://get.docker.com/ | sh
+
+echo `date` installing docker-compose
+
+apt-get install -y python
+
+curl -L https://bootstrap.pypa.io/get-pip.py | python
+pip install -U docker-compose
+
+echo `date` finished
